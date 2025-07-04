0effd626a8c2a72a1a8ca76c65a69ec6e31356f8
buddika-iresh <buddikairesh427@gmail.com>
2025-07-05 00:08:43 +0530

    Update config.js

--- config.js
+++ config.js
@@ -16,7 +16,7 @@ module.exports = {
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
     AUTO_STATUS_REPLY: process.env.AUTO_STATUS_REPLY || "false",
     AUTOLIKESTATUS: process.env.AUTOLIKESTATUS || "false",
-    ALIVE_IMG: process.env.ALIVE_IMG || "https://imgur.com/a/oa6dzbF",
+    ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/buddika-iresh17/Exsample/refs/heads/main/Photo/20250527_030737.png",
     GEMINI_API_KEY: process.env.GEMINI_API_KEY || "AIzaSyBP9qAGQUHjtIPuaZcyvSnbZDGSyHUD6bc",
     MOVIE_API_KEY: process.env.MOVIE_API_KEY || "sky|decd54b4fa030634e54d6c87fdffbb95f0bb9fb5",
     OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39" // omdbapi.com
